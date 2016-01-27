6f2d6aa749c469bd0ed879a17d800d83aa35e267
vladimir <larx83@mail.ru>
2016-01-27 22:56:49 +0300

    last

--- src/java/dao/ElementDao.java
+++ src/java/dao/ElementDao.java
@@ -9,7 +9,9 @@ import controller.ConnectionFactory;
 import entity.Elements;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -48,25 +50,20 @@ public class ElementDao implements DAO<Elements> {
 
     @Override
     public void delete(Elements t) {
-        String query = "delete from elements (elementid, name, discription,  image) values (?, ?, ?, ?)";
+        String query = "delete from where elementid = ?";
         Connection connection = null;
         try {
             connection = ConnectionFactory.getConnection();
             if (connection != null) {
                 PreparedStatement preparedStatement = connection.prepareStatement(query);
                 preparedStatement.setInt(1, t.getElementid());
-                preparedStatement.setString(2, t.getName());
-                preparedStatement.setString(3, t.getDescription());
-                preparedStatement.setString(4, t.getImage());
                 preparedStatement.execute();
             }
         } catch (SQLException ex) {
         } finally {
-
             try {
                 connection.close();
             } catch (SQLException ex) {
-
             }
 
         }
@@ -74,6 +71,34 @@ public class ElementDao implements DAO<Elements> {
 
     @Override
     public List getAll() {
+        List<Elements> objectCollection = new ArrayList();
+        String query = ("select * from elements");
+        Connection connection = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet rezult = statement.executeQuery();
+            while (rezult.next()) {
+                Elements Object = new Elements();
+
+                Object.setElementid(rezult.getInt(1));
+                Object.setName(rezult.getString(2));
+                Object.setDescription(rezult.getString(3));
+                Object.setImage(rezult.getString(4));
+                objectCollection.add(Object);
+            }
+
+        } catch (Exception ex) {
+
+        }
+        finally{
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ElementDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+}
+        return objectCollection;
     }
 
     @Override
