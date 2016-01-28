e9cc6e7ca4fec607a193631106f5b921aa8295ca
User <User@comp354>
2016-01-28 14:43:57 +0300

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/java/dao/ElementDao.java

--- src/java/dao/ElementDao.java
+++ src/java/dao/ElementDao.java
@@ -90,14 +90,13 @@ public class ElementDao implements DAO<Elements> {
 
         } catch (Exception ex) {
 
-        }
-        finally{
+        } finally {
             try {
                 connection.close();
             } catch (SQLException ex) {
                 Logger.getLogger(ElementDao.class.getName()).log(Level.SEVERE, null, ex);
             }
-}
+        }
         return objectCollection;
     }
 
@@ -107,108 +106,31 @@ public class ElementDao implements DAO<Elements> {
 
     @Override
     public Elements getById(int id) {
-        String query = "delete from elements where ";
+        String query = "select from elements where elementid=?";
         Connection connection = null;
+        Elements obj = new Elements();
         try {
             connection = ConnectionFactory.getConnection();
             if (connection != null) {
                 PreparedStatement preparedStatement = connection.prepareStatement(query);
-                preparedStatement.setInt(1, t.getElementid());
-                preparedStatement.setString(2, t.getName());
-                preparedStatement.setString(3, t.getDescription());
-                preparedStatement.setString(4, t.getImage());
-                preparedStatement.execute();
-            }
-        } catch (SQLException ex) {
-        } finally {
+                preparedStatement.setInt(1, id);
 
-            try {
-                connection.close();
-            } catch (SQLException ex) {
+                ResultSet rezult = preparedStatement.executeQuery();
 
-            }
+                if (rezult.next()) {
 
-        }
-    }
-
-    @Override
-    public List getAll() {
-        List<Elements> objectCollection = new ArrayList();
-        String query = "select * from elements";
-        Connection connection = null;
-        connection = ConnectionFactory.getConnection();
-        if (connection != null) {
-            try {
-                PreparedStatement preparedStatement = connection.prepareStatement(query);
-                PreparedStatement statement = connection.prepareStatement(query);
-                ResultSet rezult = statement.executeQuery();
-                while (rezult.next()) {
-                    Elements Object = new Elements();
-
-                    Object.setElementid(rezult.getInt(1));
-                    Object.setName(rezult.getString(2));
-                    Object.setDescription(rezult.getString(3));
-                    Object.setImage(rezult.getString(4));
-                    objectCollection.add(Object);
+                    obj.setElementid(rezult.getInt(1));
+                    obj.setName(rezult.getString(2));
+                    obj.setDescription(rezult.getString(3));
+                    obj.setImage(rezult.getString(4));
+                    return obj;
                 }
-            } catch (SQLException ex) {
-                System.out.println("Can't get all");
-            } finally {
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(ElementDao.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
             }
-
-        }
-        return objectCollection;
-    }
-
-    @Override
-    public void update(Elements t) {
-         String query = ("update elements set name=?,  description=?, image=? where elementid=?");
-        try {
-
-            Connection connection = ConnectionFactory.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(4, t.getElementid());
-            preparedStatement.setString(1, t.getName());
-            preparedStatement.setString(2, t.getDescription());
-            preparedStatement.setString(3, t.getImage());
-            preparedStatement.execute();
             connection.close();
-        } catch (Exception ex) {
-        }
-        
-    }
-
-    @Override
-    public Elements getById(int id) {
-         Elements Object = new Elements();
-        String query = ("select * from elements where elenemtid=?");
-        try {
-            Connection connection = ConnectionFactory.getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, id);
-
-            ResultSet rezult = statement.executeQuery();
-
-            if (rezult.next()) {
-
-                Object.setElementid(rezult.getInt(1));
-                Object.setName(rezult.getString(2));
-                Object.setDescription(rezult.getString(3));
-                Object.setImage(rezult.getString(4));
-
-                return Object;
-            } else {
-                return null;
-            }
-        } catch (Exception ex) {
+        } catch (SQLException ex) {
             return null;
         }
+
     }
 
 }
